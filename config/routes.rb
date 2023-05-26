99094b083ec6c787e4f466e29a360d7a8379e4a1
Mohammad Naqvi <mohammadnaqvi25@gmail.com>
2023-05-26 11:54:09 -0400

    setup project

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'api/test', to: 'application#test'
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
