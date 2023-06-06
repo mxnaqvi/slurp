b89db4b1d19e7e193857bb9dce1a2790a2c0f123
Mohammad Naqvi <mohammadnaqvi25@gmail.com>
2023-06-06 14:52:27 -0400

    render ready i think?

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,4 +1,5 @@
-class API::ReviewsController < ApplicationController
+class Api::ReviewsController < ApplicationController
+
     before_action :require_logged_in, only: [:create, :update, :destroy]
 
     def index
